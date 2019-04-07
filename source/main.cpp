541d810d3f61f61a9d693804d4f73547deab0173
Nao Taco <naotaco@gmail.com>
2019-04-08 02:10:55 +0900

    update

--- source/main.cpp
+++ source/main.cpp
@@ -9,7 +9,32 @@ constexpr int test02() { return 44; }
 
 constexpr int test03() { return test01() * test02(); }
 
+class A {
+public:
+  int a;
+  int get() { return a; }
+  void dump() { std::cout << "a : " << std::hex << &a << std::endl; }
+
+  // コピーコンストラクター
+  A(const A &) = delete;
+  // コピー代入演算子
+  A &operator=(const A &) = delete;
+
+  // デフォルトコンストラクター
+  A() = delete;
+  A(int _a) : a(_a) {}
+  // ムーブコンストラクター
+//  A(A &&) {}
+  // ムーブ代入演算子
+//  A &operator=(A &&) {}
+};
+
 int main() {
-  std::cout << test03();
+  A a(3);
+  a.dump();
+  A&& b = std::move(a);
+
+  b.dump();
+  a.dump();
   return 0;
 }
